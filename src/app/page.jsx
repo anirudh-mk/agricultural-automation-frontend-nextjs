897a270e0acca497ce260eb8c6cfe96fa911907e
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-04-19 07:22:09 +0530

    feat(login page)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import style from './page.module.css'
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className={style.loginPage}>
+      <h1>Login</h1>
+      <form action="">
+        <label htmlFor="">Username</label>
+        <br />
+        <input type="text" />
+        <br />
+        <label htmlFor="">Password</label>
+        <br />
+        <input type="password" />
+        <br />
+        <div className={style.buttonContainer}>
+          <label>Forgot password?</label>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
